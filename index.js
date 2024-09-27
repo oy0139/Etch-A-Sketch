0925db06af2b73e286abd22e9a5d3e53b8487ad5
Roy Lopez <150288003+oy0139@users.noreply.github.com>
2024-09-26 21:30:52 -0400

    Layout the grid for the sketchpad
    Run JS code after nodes are created in the DOM so the 16x16
    grid is created properly.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const container = document.querySelector(".container");
+let squares = document.createElement("div");
+
+container.style.border = "1px solid black";
+container.style.height = "300px";
+container.style.width = "500px";
+
+squares.style.border = "1px solid black";
+squares.style.width = "100px";
+squares.style.height = "200px"
+
+container.appendChild(squares);
 
