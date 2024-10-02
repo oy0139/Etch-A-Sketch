62142190e7f68a80c7501efd33d0e4a0abb51989
Roy Lopez <150288003+oy0139@users.noreply.github.com>
2024-10-02 00:15:33 -0400

    Add hover and click-to-delete features to grid
    - Implemented hover functionality that allows users to draw on the sketchpad while hovering over the grid.
    - Added the ability to click squares to delete drawings.

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const container = document.querySelector(".container");
 for (let i = 0;  i < 256; i++) {
     let squares = document.createElement("div");
 
-    squares.style.border = "2px solid black";
+    squares.style.border = "1px solid black";
     squares.style.width = "14px";
     squares.style.height = "14px";
 
+    squares.addEventListener("mouseover", () => {
+        squares.style.backgroundColor = "blue";
+    })
+    
+    squares.addEventListener("click", () => {
+        squares.style.backgroundColor = "";
+    })
+
     container.appendChild(squares);
+
 }
-
-
